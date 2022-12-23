8c85523781b489d57c8db41b0f9429b8ca3f8d27
ricardopg8 <ricardopg8@gmail.com>
2022-12-23 00:18:42 -0300

    Project createad

---
+++ SalesWebMVC05/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SalesWebMVC05.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
